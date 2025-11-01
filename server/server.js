052713fd9c30b26f103781254c8f5600e001bf6c
Rajesh-Waghmode <garryneon07@gmail.com>
2025-11-01 15:14:12 +0530

    some authenticaion code added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 dotenv.config();
@@ -9,6 +10,13 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 await connectDB();
+app.use(clerkMiddleware());
+
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+})
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
